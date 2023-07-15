cbade946bf6503cd7c8baf329679856c9601a3f3
Victor <wanjala94vic@gmail.com>
2023-07-15 16:34:19 +0300

    Completed assignment

---
+++ src/00-objectball.js
@@ -0,0 +1,48 @@
+function gameObject() {
+    return {
+      home: {
+        teamName: "Brooklyn Nets",
+        colors: ["Black", "White"],
+        players: {
+          "Alan Anderson": {
+            number: 0,
+            shoe: 16,
+            points: 22,
+            rebounds: 12,
+            assists: 12,
+            steals: 3,
+            blocks: 1,
+            slamDunks: 1,
+          },
+          "Reggie Evans": {
+            number: 30,
+            shoe: 14,
+            points: 12,
+            rebounds: 12,
+            assists: 12,
+            steals: 12,
+            blocks: 12,
+            slamDunks: 7,
+          },
+        
+        },
+      },
+      away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players: {
+        },
+      },
+    };
+  }
+
+  function homeTeamName() {
+    const gameObj = gameObject();
+    return gameObj.home.teamName;
+  }
+
+  console.log(homeTeamName());
+
+
+  
+  
